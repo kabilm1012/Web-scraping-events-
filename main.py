1dbb73051abc41f4938a4dc9c51c60731f6eb596
Kabil M <kabilm1012@gmail.com>
2024-09-26 15:05:33 +0530

    store data in text

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-import requests
-import selectorlib
+from webscrape import scrape, extract
+from textdata import read, store
+from send_email import send_email
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 
-def scrape(url):
-    """Scrape the page source from the URL"""
-    response = requests.get(url, headers=HEADERS)
-    source = response.text
-    return source
-
-
-def extract(source):
-    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)["tours"]
-    return value
-
-
 if __name__ == "__main__":
-    scraped = scrape(URL)
+    scraped = scrape(URL, HEADERS)
     extracted = extract(scraped)
     print(extracted)
+    data = read("data.txt")
+    if extracted != "No upcoming tours":
+        if extracted not in data:
+            store("data.txt", extracted)
+            send_email()
